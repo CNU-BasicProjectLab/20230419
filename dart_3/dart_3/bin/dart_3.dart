c8297d48eea3aae10108d75ccb1e8f30f3ab6e40
picbuddy <picbuddy@naver.com>
2023-04-18 15:03:33 +0900

    3

--- dart_3/dart_3/bin/dart_3.dart
+++ dart_3/dart_3/bin/dart_3.dart
@@ -2,7 +2,7 @@
 bool isEmpty(String? string) => string?.length == 0;
 
 main() {
-  bool b = isEmpty(null);
+  bool b = isEmpty(null); // 자료형 다음에 ?를 붙이면 Null이 가능함
   print('null is Empty = $b');
 
   b = isEmpty("");
